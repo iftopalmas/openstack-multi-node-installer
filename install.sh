8597e92b9fe41fd8d00c1c7f1b0cbaecd9ee84dc
Manoel Campos <manoelcampos@gmail.com>
2018-10-03 11:06:58 -0300

    Fix user directory creation

--- install.sh
+++ install.sh
@@ -30,19 +30,19 @@ fi
 apt-get install -y git
 
 USERNAME="stack"
-cp *.sh /opt/$USERNAME && chmod a+x /opt/$USERNAME/*.sh
-
 useradd -s /bin/bash -d /opt/$USERNAME -m $USERNAME
+
+cp *.sh /opt/$USERNAME && chmod a+x /opt/$USERNAME/*.sh
 
 #Gives user root permission if it has not been given yet
 cat /etc/sudoers | grep "$USERNAME ALL=(ALL) NOPASSWD: ALL" > /dev/null || echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 if [[ $1 == "-c" || $1 == "--controller" ]]; then
-  sudo -i -u $USERNAME ./openstack-controller.sh
+   sudo -i -u $USERNAME ./openstack-controller.sh
 elif [[ $1 == "-n" || $1 == "--node" ]]; then
-  #SERVICE_HOST = IP of the open stack controller
-  SERVICE_HOST=$2
-  sudo -i -u $USERNAME ./openstack-node.sh $SERVICE_HOST
+   #SERVICE_HOST = IP of the open stack controller
+   SERVICE_HOST=$2
+   sudo -i -u $USERNAME ./openstack-node.sh $SERVICE_HOST
 fi
 
 
